ff2e33fc96dd49d77fb90316528e6435e4299953
rajnish-2003 <merajnish2003@gmail.com>
2024-12-27 11:39:35 +0530

    Stop tracking client/ folder and its files

--- client/src/Redux/Store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import userReducer  from './user/userSlice';
-
-export const Store=configureStore({
-    reducer:{user:userReducer},
-    middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
-        serializableCheck:false,
-    }),
-});
